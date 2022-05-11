9fa72d328d6ad67d6206bc2cf420295cedc9b74c
Emelie Verdugo <emelieverdugo@protonmail.com>
2022-05-11 18:26:11 +0100

    Cleared up some bugs

--- src/Entity/Library.php
+++ src/Entity/Library.php
@@ -11,7 +11,7 @@ class Library
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private string $id;
+    private int $id;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private string $name;
