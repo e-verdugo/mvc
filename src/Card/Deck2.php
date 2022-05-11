1a883d7b6f8567e8ed8573dc3304565c1755a156
Emelie Verdugo <emelieverdugo@protonmail.com>
2022-05-11 22:58:49 +0100

    Optimized some code

--- src/Card/Deck2.php
+++ src/Card/Deck2.php
@@ -9,15 +9,7 @@ class Deck2 extends Deck
 {
     public function __construct()
     {
-        for ($x = 0; $x <= 3; $x++) {
-            $suit = $this->suits[$x];
-            for ($y = 0; $y <= 13; $y++) {
-                if ($y != 11) {
-                    $card = new Card($y, $suit);
-                    array_push($this->deck, $card);
-                }
-            }
-        }
+        parent::__construct();
         $card = new Card(14, "hearts");
         array_push($this->deck, $card);
         array_push($this->deck, $card);
